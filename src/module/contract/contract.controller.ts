49a5b4e388237bb687c0fed0fc4119ba92dfee8d
rana16468 <amsohelrana.me@gmail.com>
2026-02-02 03:28:38 +0600

    sgdahfgasd sdajgfasd

--- src/module/contract/contract.controller.ts
+++ src/module/contract/contract.controller.ts
@@ -4,7 +4,8 @@ import { ContractService } from "./contract.services";
 
 import httpStatus from "http-status";
 import catchAsync from "../../utility/catchAsync";
-import sendRespone from "../../utility/sendResponse";
+import sendResponse from "../../utility/sendResponse";
+
 
 
 
@@ -13,7 +14,7 @@ const createContract:RequestHandler=catchAsync(async(req,res)=>{
     
 
     const result=await ContractService.createContractIntoDb(req.body);
-    sendRespone(res,{success:true,statusCode:httpStatus.CREATED,message:"Sucessfulled Added Contract",data:result});
+    sendResponse(res,{success:true,statusCode:httpStatus.CREATED,message:"Sucessfulled Added Contract",data:result});
 
 
 });
@@ -22,21 +23,21 @@ const createContract:RequestHandler=catchAsync(async(req,res)=>{
 const AllContract:RequestHandler=catchAsync(async(req,res)=>{
 
     const result=await ContractService.AllContractIntoDb();
-    sendRespone(res,{success:true,statusCode:httpStatus.OK,message:"Successfully Find All Conreact",data:result});
+    sendResponse(res,{success:true,statusCode:httpStatus.OK,message:"Successfully Find All Conreact",data:result});
 
 });
 
 const  SpecificContractId:RequestHandler=catchAsync(async(req,res)=>{
     const {id}=req.params;
     const result=await ContractService.SpecificContractIdIntoDb(id);
-    sendRespone(res,{success:true,statusCode:httpStatus.OK,message:"Successfully Get Specific Contract",data:result});
+    sendResponse(res,{success:true,statusCode:httpStatus.OK,message:"Successfully Get Specific Contract",data:result});
 });
 
 const UpdateContract:RequestHandler=catchAsync(async(req,res)=>{
 
     const {id}=req.params;
     const result=await ContractService.UpdateContractFromDb(id,req.body);
-    sendRespone(res,{success:true,statusCode:httpStatus.OK,message:"Successfully Updated Contract Information",data:result});
+    sendResponse(res,{success:true,statusCode:httpStatus.OK,message:"Successfully Updated Contract Information",data:result});
 
 });
 
@@ -44,13 +45,13 @@ const DeleteContract:RequestHandler=catchAsync(async(req,res)=>{
 
     const {id}=req.params;
     const result=await ContractService.DeleteContractFromDb(id);
-    sendRespone(res,{success:true,statusCode:httpStatus.OK,message:"Successfully Delete Contrcat Info",data:result});
+    sendResponse(res,{success:true,statusCode:httpStatus.OK,message:"Successfully Delete Contrcat Info",data:result});
 
 });
 const FavoriteContrcat:RequestHandler=catchAsync(async(req,res)=>{
     const {id}=req.params;
     const result=await ContractService.FavoriteContrcatFromDb(id);
-    sendRespone(res,{success:true,statusCode:httpStatus.OK,message:"Successfully Recorded Fevorite",data:result});
+    sendResponse(res,{success:true,statusCode:httpStatus.OK,message:"Successfully Recorded Fevorite",data:result});
 })
 
 export const ContractController={
